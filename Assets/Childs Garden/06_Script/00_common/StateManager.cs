14f2eaa96fea2bbb8b47abccea58e4ee8b173730
bolcof <sanctum.crusade.ex@gmail.com>
2023-09-14 22:06:48 +0900

    これはどうせ使うようにしたい

---
+++ Assets/Childs Garden/06_Script/00_common/StateManager.cs
@@ -0,0 +1,33 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour {
+    public static StateManager instance;
+
+    //使用しているSpawn PositionのID
+    public int mySpawnPositionId;
+
+    //対戦人数
+    public int PlayerNum = 2;
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+        mySpawnPositionId = -1;
+    }
+
+    //PlayerのRoom内ID
+    public int MyPlayerId() {
+        if (PhotonNetwork.connected && PhotonNetwork.inRoom) {
+            return PhotonNetwork.player.ID;
+        } else {
+            Debug.LogError("Out of Connect or Room!");
+            return -1;
+        }
+    }
+}
