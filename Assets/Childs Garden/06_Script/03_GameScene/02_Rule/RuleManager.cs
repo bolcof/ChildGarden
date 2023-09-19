a6c36abeaacdc96b89d573a48de37dd2a48126ec
bolcof <sanctum.crusade.ex@gmail.com>
2023-09-19 23:13:30 +0900

    RuleManagerというのを作る

---
+++ Assets/Childs Garden/06_Script/03_GameScene/02_Rule/RuleManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuleManager : Photon.PunBehaviour {
+
+    public struct Rule {
+        public int id;
+        public string checkMethodName;
+        public string explainText;
+    }
+
+    public List<Rule> rules;
+
+    //TODO 複数になるかも
+    public Rule currentRule;
+
+    private void Update() {
+        switch (currentRule.id) {
+            case -1:
+                Debug.LogError("Rule is not set yet!");
+                break;
+            case 1:
+                CheckRule1();
+                break;
+            default:
+                Debug.LogError("RuleID is out of range!");
+                break;
+        }
+    }
+
+    //これが無いと動くけどエラーが出る
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+        if (stream.isWriting) {
+            // ここにオブジェクトの状態を送信するコードを書きます
+        } else {
+            // ここにオブジェクトの状態を受信して更新するコードを書きます
+        }
+    }
+
+    //Ruleごとに作る
+    public bool CheckRule1() {
+        return false;
+    }
+}
