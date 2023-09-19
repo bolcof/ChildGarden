605c835a39ca2dc5965c5a98a4cabe521a79a258
bolcof <sanctum.crusade.ex@gmail.com>
2023-09-19 22:13:24 +0900

    これで動く

--- Assets/Childs Garden/06_Script/03_GameScene/01_MainManager/GameManager.cs
+++ Assets/Childs Garden/06_Script/03_GameScene/01_MainManager/GameManager.cs
@@ -6,4 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : Photon.PunBehaviour {
 
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+        //これが無いと動くけどエラーが出る
+        if (stream.isWriting) {
+            // ここにオブジェクトの状態を送信するコードを書きます
+        } else {
+            // ここにオブジェクトの状態を受信して更新するコードを書きます
+        }
+    }
 }
