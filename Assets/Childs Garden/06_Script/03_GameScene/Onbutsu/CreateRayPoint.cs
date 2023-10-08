694dfb214770dd964604550ee023654de04d949a
bolcof <sanctum.crusade.ex@gmail.com>
2023-10-08 20:28:44 +0900

    チャージ中の音は少なくともできた

--- Assets/Childs Garden/06_Script/03_GameScene/Onbutsu/CreateRayPoint.cs
+++ Assets/Childs Garden/06_Script/03_GameScene/Onbutsu/CreateRayPoint.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon;
 using UnityEngine.UI;
+using Unity.Burst.CompilerServices;
 
 public class CreateRayPoint : Photon.PunBehaviour {
     [SerializeField]
@@ -18,10 +19,12 @@ public class CreateRayPoint : Photon.PunBehaviour {
     [SerializeField] List<GameObject> ChargingEffects = new List<GameObject>();
 
     [SerializeField] private float chargingTime;
+    private int currentChargeLevel, pastChargeLevel;
     [SerializeField] private float levelUpTime;
 
     [SerializeField] private Slider chargeSlider;
     [SerializeField] private GameObject sizeSignKnob;
+    //[SerializeField] private List<GameObject> chargeEffectObject;
     [SerializeField] private List<Color> sliderColors = new List<Color>();
 
     private void Start() {
@@ -29,37 +32,67 @@ public class CreateRayPoint : Photon.PunBehaviour {
         chargingTime = 0.0f;
         sizeSignKnob.SetActive(false);
         chargeSlider.gameObject.SetActive(false);
+        currentChargeLevel = -1;
+        pastChargeLevel = -1;
+
+        /*foreach (var ef in chargeEffectObject) {
+            ef.SetActive(false);
+        }*/
     }
 
     void Update() {
         if (GameManager.Instance.canPutOnbutsu) {
-            int level = FloatDivide(chargingTime, levelUpTime);
+
+            if (Input.GetMouseButtonDown(0)) {
+                pastChargeLevel = 0;
+                currentChargeLevel = 0;
+                ChargeLevelUp();
+            }
 
             if (Input.GetMouseButton(0)) {
+                //TODO:èáî‘êÆóù
                 chargingTime += Time.deltaTime;
                 chargeSlider.gameObject.SetActive(true);
                 sizeSignKnob.SetActive(true);
 
-                chargeSlider.fillRect.GetComponent<Image>().color = sliderColors[level];
-                sizeSignKnob.GetComponent<Image>().color = sliderColors[level];
-                if (level <= 2) {
+                currentChargeLevel = FloatDivide(chargingTime, levelUpTime);
+                if (currentChargeLevel >= 3) { currentChargeLevel = 3; }
+
+                Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, rayDistance)) {
+                    Vector3 effectPosition = hit.point + Vector3.up;
+                    //chargeEffectObject[currentChargeLevel].transform.position = effectPosition;
+                }
+
+                chargeSlider.fillRect.GetComponent<Image>().color = sliderColors[currentChargeLevel];
+                sizeSignKnob.GetComponent<Image>().color = sliderColors[currentChargeLevel];
+                if (currentChargeLevel <= 2) {
                     chargeSlider.value = FloatDivideRemain(chargingTime, levelUpTime) / levelUpTime * 1000;
                 } else {
                     chargeSlider.value = 1000f;
                 }
+
+                if(pastChargeLevel != currentChargeLevel) {
+                    ChargeLevelUp();
+                }
+
+                pastChargeLevel = currentChargeLevel;
             }
 
             if (Input.GetMouseButtonUp(0)) {
                 SoundManager.Instance.StopSoundEffect();
                 SoundManager.Instance.PlaySoundEffect(SoundManager.Instance.SE_PutOnbutsu);
                 chargingTime = 0.0f;
+                SoundManager.Instance.ChargeSource.Stop();
 
                 Ray ray = camera.ScreenPointToRay(Input.mousePosition);
                 RaycastHit hit;
 
                 if (Physics.Raycast(ray, out hit, rayDistance)) {
                     Vector3 spawnPosition = hit.point + Vector3.up * 0.5f;
-                    switch (level) {
+                    switch (currentChargeLevel) {
                         case 0:
                             PhotonNetwork.Instantiate(onbutsuFolderName + OnbutsuList_Level1[PhotonNetwork.player.ID - 1].name, spawnPosition, Quaternion.identity, 0);
                             break;
@@ -81,7 +114,12 @@ public class CreateRayPoint : Photon.PunBehaviour {
     }
 
     private void ChargeLevelUp() {
-
+        if (currentChargeLevel > 3) { currentChargeLevel = 3; }
+        /*foreach (var ef in chargeEffectObject) {
+            ef.SetActive(false);
+        }*/
+        //chargeEffectObject[currentChargeLevel].SetActive(true);
+        SoundManager.Instance.ChangeChargeEffect(currentChargeLevel);
     }
 
     private int FloatDivide(float n1, float n2) {
