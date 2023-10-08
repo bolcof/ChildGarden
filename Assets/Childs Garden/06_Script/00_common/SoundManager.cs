037b517937e9ff6e9810e5cacb8ea1796896862e
bolcof <sanctum.crusade.ex@gmail.com>
2023-10-08 12:55:07 +0900

    Sounds

--- Assets/Childs Garden/06_Script/00_common/SoundManager.cs
+++ Assets/Childs Garden/06_Script/00_common/SoundManager.cs
@@ -12,7 +12,7 @@ public class SoundManager : MonoBehaviour {
     [SerializeField] public AudioClip SE_CountDown;
     [SerializeField] public AudioClip SE_Progress;
     [SerializeField] public AudioClip SE_PutOnbutsu;
-    [SerializeField] public AudioClip SE_OnbutsuCharging;
+    [SerializeField] public List<AudioClip> SE_OnbutsuCharging = new List<AudioClip>();
     [SerializeField] public AudioClip SE_ChargeLevelUp;
     [SerializeField] public AudioClip SE_RoundFinish;
     [SerializeField] public AudioClip SE_SpawnZizou;
@@ -21,9 +21,11 @@ public class SoundManager : MonoBehaviour {
     [SerializeField] public AudioClip SE_RuleSelectViewOpening; /* to webm */
 
     [SerializeField] public AudioClip BGM_TitleAndRule;
-    [SerializeField] public AudioClip BGM_GameScene;
+    [SerializeField] public List<AudioClip> BGM_GameScene = new List<AudioClip>();
     [SerializeField] public AudioClip BGM_RuleSelect;
     [SerializeField] public AudioClip BGM_Ending;
+
+    [SerializeField] public AudioSource BgmSource, SeSource;
 
     private void Awake() {
         if (Instance == null) {
@@ -32,5 +34,19 @@ public class SoundManager : MonoBehaviour {
         } else {
             Destroy(gameObject);
         }
+    }
+
+    public void PlaySoundEffect(AudioClip sound) {
+        SeSource.PlayOneShot(sound);
+    }
+
+    public void PlayBgm(AudioClip bgm) {
+        BgmSource.clip = bgm;
+        BgmSource.Play();
+    }
+
+    public void StopSoundEffect() {
+        SeSource.loop = false;
+        SeSource.Stop();
     }
 }
--- Assets/Childs Garden/06_Script/02_RuleScene/TextBoxControl.cs
+++ Assets/Childs Garden/06_Script/02_RuleScene/TextBoxControl.cs
@@ -1,30 +1,26 @@
 using UnityEngine;
 
-public class TextBoxControl : MonoBehaviour
-{
+public class TextBoxControl : MonoBehaviour {
     public GameObject[] objects; // インスペクターでオブジェクトの配列を指定します。
     private int currentIndex = 0; // 現在アクティブなオブジェクトのインデックス。
     public GameObject fripObj;
     public GameObject startObj;
 
-    private void Start()
-    {
+    private void Start() {
         // スタート時に全てのオブジェクトを非アクティブにします
-        for (int i = 0; i < objects.Length; i++)
-        {
+        for (int i = 0; i < objects.Length; i++) {
             objects[i].SetActive(false);
         }
 
         // 最初のオブジェクトをアクティブにします
-        if(objects.Length > 0)
-        {
+        if (objects.Length > 0) {
             objects[0].SetActive(true);
         }
     }
 
     // このメソッドを呼ぶと、次のオブジェクトがアクティブになり、現在のオブジェクトは非アクティブになります。
-    public void SwitchToNextObject()
-    {
+    public void SwitchToNextObject() {
+        SoundManager.Instance.PlaySoundEffect(SoundManager.Instance.SE_RuleTextAppearing);
         // 現在のオブジェクトを非アクティブにします
         objects[currentIndex].SetActive(false);
 
@@ -34,8 +30,7 @@ public class TextBoxControl : MonoBehaviour
         // 次のオブジェクトをアクティブにします
         objects[currentIndex].SetActive(true);
 
-        if(currentIndex == 6)
-        {
+        if (currentIndex == 6) {
             fripObj.SetActive(false);
             startObj.SetActive(true);
         }
