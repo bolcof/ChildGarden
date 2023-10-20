412b36d92b4f774f38b794b79546d76924e862c2
bolcof <sanctum.crusade.ex@gmail.com>
2023-10-20 16:14:52 +0900

    Max人数

--- Assets/Childs Garden/06_Script/01_Start_Scene/RoomConector.cs
+++ Assets/Childs Garden/06_Script/01_Start_Scene/RoomConector.cs
@@ -43,6 +43,7 @@ public class RoomConector : Photon.PunBehaviour {
         Debug.Log("ルームの入室に失敗しました。");
         RoomOptions roomOptions = new RoomOptions();
         roomOptions.PublishUserId = true;
+        roomOptions.MaxPlayers = 2;
         PhotonNetwork.CreateRoom(roomId, roomOptions, TypedLobby.Default);
     }
     #endregion
