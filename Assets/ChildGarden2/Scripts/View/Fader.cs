718b517b0a8eb26e1130c246d29277f18cea9689
bolcof <sanctum.crusade.ex@gmail.com>
2023-11-13 14:14:27 +0900

    Fade

--- Assets/ChildGarden2/Scripts/View/Fader.cs
+++ Assets/ChildGarden2/Scripts/View/Fader.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class Fader : MonoBehaviour {
     [SerializeField] private Image white;
     private void Awake() {
-        
+        white.DOFade(0.0f, 0.8f);
+    }
+
+    public void WhiteOut() {
+        white.DOFade(1.0f, 1.8f);
     }
 }
