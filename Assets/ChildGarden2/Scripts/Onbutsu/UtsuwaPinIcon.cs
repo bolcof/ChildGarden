09ebdc240bca2325182c99f13263ca92447354a6
bolcof <sanctum.crusade.ex@gmail.com>
2024-01-21 15:53:15 +0900

    ピンアイコンの点滅

--- Assets/ChildGarden2/Scripts/Onbutsu/UtsuwaPinIcon.cs
+++ Assets/ChildGarden2/Scripts/Onbutsu/UtsuwaPinIcon.cs
@@ -1,7 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class UtsuwaPinIcon : MonoBehaviour {
-
+    [SerializeField] private float duration;
+    [SerializeField] private SpriteRenderer pin, anata;
+    public void StartAnimation() {
+        Debug.Log("pin Start");
+        pin.DOFade(endValue: 0f, duration: duration).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutQuint);
+        anata.DOFade(endValue: 0f, duration: duration).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutQuint);
+    }
 }
