73489919b2616764e0fac160b51d6add6cfa7711
bolcof <sanctum.crusade.ex@gmail.com>
2024-01-21 18:04:43 +0900

    ID指定を複雑に

--- Assets/ChildGarden2/Scripts/Onbutsu/CreateRayPoint.cs
+++ Assets/ChildGarden2/Scripts/Onbutsu/CreateRayPoint.cs
@@ -129,7 +129,7 @@ public class CreateRayPoint : Photon.PunBehaviour {
                             }
                             break;
                         case 1:
-                            PhotonNetwork.Instantiate(onbutsuFolderName + OnbutsuList_Level1[PhotonNetwork.player.ID - 1].name, spawnPosition, Quaternion.identity, 0);
+                            PhotonNetwork.Instantiate(onbutsuFolderName + OnbutsuList_Level1[usingOnbutsuColor[PhotonNetwork.player.ID - 1]].name, spawnPosition, Quaternion.identity, 0);
                             angelTalk_OnbutsuGenerated = false;
                             if (!angelTalk_sankakuGenerated) {
                                 angelTalk_maruCount++;
@@ -139,17 +139,17 @@ public class CreateRayPoint : Photon.PunBehaviour {
                             }
                             break;
                         case 2:
-                            PhotonNetwork.Instantiate(onbutsuFolderName + OnbutsuList_Level2[PhotonNetwork.player.ID - 1].name, spawnPosition, Quaternion.identity, 0);
+                            PhotonNetwork.Instantiate(onbutsuFolderName + OnbutsuList_Level2[usingOnbutsuColor[PhotonNetwork.player.ID - 1]].name, spawnPosition, Quaternion.identity, 0);
                             angelTalk_OnbutsuGenerated = false;
                             angelTalk_sankakuGenerated = false;
                             break;
                         case 3:
-                            PhotonNetwork.Instantiate(onbutsuFolderName + OnbutsuList_Level3[PhotonNetwork.player.ID - 1].name, spawnPosition, Quaternion.identity, 0);
+                            PhotonNetwork.Instantiate(onbutsuFolderName + OnbutsuList_Level3[usingOnbutsuColor[PhotonNetwork.player.ID - 1]].name, spawnPosition, Quaternion.identity, 0);
                             angelTalk_OnbutsuGenerated = false;
                             angelTalk_sankakuGenerated = false;
                             break;
                         default:
-                            PhotonNetwork.Instantiate(onbutsuFolderName + OnbutsuList_Level4[PhotonNetwork.player.ID - 1].name, spawnPosition, Quaternion.identity, 0);
+                            PhotonNetwork.Instantiate(onbutsuFolderName + OnbutsuList_Level4[usingOnbutsuColor[PhotonNetwork.player.ID - 1]].name, spawnPosition, Quaternion.identity, 0);
                             angelTalk_OnbutsuGenerated = false;
                             angelTalk_sankakuGenerated = false;
                             break;
