e93d39cb0ec02cec45052704aedcea09ccaf59f1
bolcof <sanctum.crusade.ex@gmail.com>
2024-02-05 19:14:06 +0900

    ピンが点滅しなくなるエラー対処

--- Assets/ChildGarden2/Scripts/Onbutsu/UtsuwaPinIcon.cs
+++ Assets/ChildGarden2/Scripts/Onbutsu/UtsuwaPinIcon.cs
@@ -7,6 +7,7 @@ public class UtsuwaPinIcon : MonoBehaviour {
     [SerializeField] private float duration;
     [SerializeField] private SpriteRenderer pin, anata;
     public void StartAnimation() {
+        pin.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
         pin.DOFade(endValue: 0f, duration: duration).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutQuint);
     }
 }
